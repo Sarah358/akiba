809ead02cd69de2125decfd33bc75306a9561671
Sarah358 <njerinjuguna358@gmail.com>
2022-05-30 13:22:10 +0300

    spring boot project

---
+++ akibaapp/src/main/java/com/akiba/akibaapp/AkibaappApplication.java
@@ -0,0 +1,13 @@
+package com.akiba.akibaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkibaappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AkibaappApplication.class, args);
+	}
+
+}
---
+++ akibaapp/src/test/java/com/akiba/akibaapp/AkibaappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akiba.akibaapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AkibaappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
